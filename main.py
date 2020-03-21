50f61f6a2f2f94084981ba942515a0cc82c4da5c
Irae Hueck Costa <mail@irae.me>
2020-03-21 18:22:18 +0100

    -

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import os
 
-import psycopg2
 from flask import Flask
 
 app = Flask(__name__)
 
 
 
-DATABASE_URL = os.environ['DATABASE_URL']
-
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if not DATABASE_URL:
+    import sqlite3
+    conn = sqlite3.connect(':memory:')
+else:
+    import psycopg2
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
