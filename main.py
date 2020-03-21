f00dcec625351d9c144b2a3260d7346af5bc7987
Irae Hueck Costa <mail@irae.me>
2020-03-21 18:13:42 +0100

    -

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import os
+
+import psycopg2
 from flask import Flask
+
 app = Flask(__name__)
+
+
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
 
 @app.route('/')
 def hello_world():
